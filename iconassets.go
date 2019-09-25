04101acb2980356af2d516e627aea1d6f61b08d7
loki verloren <l0k18@protonmail.com>
2019-09-25 15:21:11 +0200

    best working with nice logger
    Former-commit-id: e6e3ec51d571503a9dbb68e3c7dc43da8e1e4bb5

---
+++ iconassets.go
@@ -0,0 +1,40 @@
+package jdb
+	
+var IcoLogo = Base64ToBytes("")
+
+var IcoReceive = Base64ToBytes("")
+
+var IcoReceived = Base64ToBytes("")
+
+var IcoOverview = Base64ToBytes("")
+
+var IcoSettings = Base64ToBytes("")
+
+var IcoSend = Base64ToBytes("")
+
+var IcoSent = Base64ToBytes("")
+
+var IcoHistory = Base64ToBytes("")
+
+var IcoHelp = Base64ToBytes("")
+
+var IcoInfo = Base64ToBytes("")
+
+var IcoPeers = Base64ToBytes("")
+
+var IcoBlocks = Base64ToBytes("")
+
+var IcoBalance = Base64ToBytes("")
+
+var IcoLink = Base64ToBytes("")
+
+var IcoLinkOff = Base64ToBytes("")
+
+var IcoLoading = Base64ToBytes("")
+
+var IcoAddressBook = Base64ToBytes("")
+
+var IcoUnconfirmed = Base64ToBytes("")
+
+var IcoTxNumber = Base64ToBytes("")
+
