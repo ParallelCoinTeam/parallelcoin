650cdc4deeb2b472fe64943ceeb1d624739740de
loki verloren <l0k18@protonmail.com>
2019-10-01 00:27:16 +0200

    started with block subscriber and pauser
    Former-commit-id: f111430a089ef1791460f8ffcfb447f8390e4503

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -2,23 +2,30 @@ package controller
 
 import (
 	"context"
+	"math/rand"
+	"sync/atomic"
+	"time"
 
+	blockchain "github.com/p9c/pod/pkg/chain"
+	"github.com/p9c/pod/pkg/chain/fork"
 	"github.com/p9c/pod/pkg/chain/mining"
 	"github.com/p9c/pod/pkg/conte"
+	"github.com/p9c/pod/pkg/log"
 )
 
-type Blocks []mining.BlockTemplate
+type Blocks []*mining.BlockTemplate
 
 func Run(cx *conte.Xt) (cancel context.CancelFunc) {
 	var ctx context.Context
-	var lastBlock Blocks
+	var lastBlock atomic.Value // Blocks
 	ctx, cancel = context.WithCancel(context.Background())
 	blockChan := make(chan Blocks)
 	go func() {
 		for {
 			// work loop
 			select {
-			case lastBlock = <-blockChan:
+			case lb := <-blockChan:
+				lastBlock.Store(lb)
 				// send out block broadcast
 			case <-ctx.Done():
 				// cancel has been called
@@ -28,8 +35,44 @@ func Run(cx *conte.Xt) (cancel context.CancelFunc) {
 		}
 	}()
 	// create subscriber for new block event
-
+	cx.RPCServer.Cfg.Chain.Subscribe(func(n *blockchain.
+	Notification) {
+		switch n.Type {
+		case blockchain.NTBlockConnected:
+			lastBlock.Store(Blocks{})
+			// generate Blocks
+			for algo := range fork.List[fork.GetCurrent(cx.RPCServer.Cfg.Chain.
+				BestSnapshot().Height+1)].Algos {
+				// Choose a payment address at random.
+				rand.Seed(time.Now().UnixNano())
+				payToAddr := cx.StateCfg.ActiveMiningAddrs[rand.Intn(len(cx.
+					StateCfg.ActiveMiningAddrs))]
+				template, err := cx.RPCServer.Cfg.Generator.NewBlockTemplate(0,
+					payToAddr, algo)
+				if err != nil {
+					log.ERROR("failed to create new block template:", err)
+					continue
+				}
+				lastBlock.Store(append(lastBlock.Load().(Blocks), template))
+			}
+			blockChan <- lastBlock.Load().(Blocks)
+		}
+	})
 	// goroutine loop checking for connection and sync status
-	
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			connCount := cx.RPCServer.Cfg.ConnMgr.ConnectedCount()
+			current := cx.RPCServer.Cfg.SyncMgr.IsCurrent()
+			if connCount < 1 || !current {
+				lastBlock.Store(Blocks{})
+				blockChan <- lastBlock.Load().(Blocks)
+			}
+			select {
+			case <-ctx.Done():
+				break
+			}
+		}
+	}()
 	return
 }
