31a934caf37db380e35219ee5cdb761d06b3a190
loki verloren <l0k18@protonmail.com>
2019-09-30 22:24:51 +0200

    sketched out controller

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,2 +1,35 @@
 package controller
 
+import (
+	"context"
+
+	"github.com/p9c/pod/pkg/chain/mining"
+	"github.com/p9c/pod/pkg/conte"
+)
+
+type Blocks []mining.BlockTemplate
+
+func Run(cx *conte.Xt) (cancel context.CancelFunc) {
+	var ctx context.Context
+	var lastBlock Blocks
+	ctx, cancel = context.WithCancel(context.Background())
+	blockChan := make(chan Blocks)
+	go func() {
+		for {
+			// work loop
+			select {
+			case lastBlock = <-blockChan:
+				// send out block broadcast
+			case <-ctx.Done():
+				// cancel has been called
+				return
+			default:
+			}
+		}
+	}()
+	// create subscriber for new block event
+
+	// goroutine loop checking for connection and sync status
+	
+	return
+}
