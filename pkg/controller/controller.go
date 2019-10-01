421be6beb7099507773a09ae5b8db8bf2ebedc99
loki verloren <l0k18@protonmail.com>
2019-10-01 15:18:43 +0200

    ready to put in block dispatch
    Former-commit-id: 73ea70f679a34a0eb20198d0a125339bbf49fdcd

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -16,6 +16,7 @@ import (
 type Blocks []*mining.BlockTemplate
 
 func Run(cx *conte.Xt) (cancel context.CancelFunc) {
+	log.WARN("starting controller")
 	var ctx context.Context
 	var lastBlock atomic.Value // Blocks
 	ctx, cancel = context.WithCancel(context.Background())
@@ -28,6 +29,7 @@ func Run(cx *conte.Xt) (cancel context.CancelFunc) {
 				lastBlock.Store(lb)
 				// send out block broadcast
 				log.WARN("sending out block broadcast")
+				// log.DEBUG(spew.Sdump(lb))
 			case <-ctx.Done():
 				// cancel has been called
 				return
@@ -35,11 +37,49 @@ func Run(cx *conte.Xt) (cancel context.CancelFunc) {
 			}
 		}
 	}()
+	connCount := cx.RPCServer.Cfg.ConnMgr.ConnectedCount()
+	current := cx.RPCServer.Cfg.SyncMgr.IsCurrent()
+	// if out of sync or disconnected,
+	// once a second send out empty blocks
+	for (connCount < 1 && !*cx.Config.Solo) || !current {
+		time.Sleep(time.Second)
+		connCount = cx.RPCServer.Cfg.ConnMgr.ConnectedCount()
+		current = cx.RPCServer.Cfg.SyncMgr.IsCurrent()
+		log.WARN("waiting for sync/peers", connCount, current)
+		select {
+		case <-ctx.Done():
+			log.WARN("cancelled before initial connection/sync")
+			return
+		default:
+		}
+	}
+	// load initial blocks and send them out
+	lastBlock.Store(Blocks{})
+	var blocks Blocks
+	// generate Blocks
+	for algo := range fork.List[fork.GetCurrent(cx.RPCServer.Cfg.Chain.
+		BestSnapshot().Height+1)].Algos {
+		// Choose a payment address at random.
+		rand.Seed(time.Now().UnixNano())
+		payToAddr := cx.StateCfg.ActiveMiningAddrs[rand.Intn(len(cx.
+			StateCfg.ActiveMiningAddrs))]
+		template, err := cx.RPCServer.Cfg.Generator.NewBlockTemplate(0,
+			payToAddr, algo)
+		if err != nil {
+			log.ERROR("failed to create new block template:", err)
+			continue
+		}
+		blocks = append(blocks, template)
+	}
+	lastBlock.Store(blocks)
+	log.WARN("sending initial block broadcast")
+	blockChan <- lastBlock.Load().(Blocks)
 	// create subscriber for new block event
 	cx.RPCServer.Cfg.Chain.Subscribe(func(n *chain.
 	Notification) {
 		switch n.Type {
 		case chain.NTBlockConnected:
+			log.WARN("new block found")
 			lastBlock.Store(Blocks{})
 			var blocks Blocks
 			// generate Blocks
