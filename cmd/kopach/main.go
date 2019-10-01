73ea70f679a34a0eb20198d0a125339bbf49fdcd
loki verloren <l0k18@protonmail.com>
2019-10-01 15:18:43 +0200

    ready to put in block dispatch

--- cmd/kopach/main.go
+++ cmd/kopach/main.go
@@ -4,22 +4,18 @@ import (
 	"sync"
 
 	"github.com/p9c/pod/pkg/conte"
-	"github.com/p9c/pod/pkg/controller"
 )
 
 func Main(cx *conte.Xt, quit chan struct{}, wg *sync.WaitGroup) {
-	cancel := controller.Run(cx)
 	go func() {
 		wg.Add(1)
 	out:
 		for {
 			select {
 			case <-quit:
-				cancel()
 				break out
 			}
 		}
 		wg.Done()
 	}()
-
 }
