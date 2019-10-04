99fa004d2d3d883a985f2322f08930638944574f
loki verloren <l0k18@protonmail.com>
2019-10-04 15:21:02 +0200

    kopach prints received packets
    Former-commit-id: 1462f1ff536605e95fff9f0351693a2fa8523f2b

--- cmd/kopach/main.go
+++ cmd/kopach/main.go
@@ -1,7 +1,10 @@
 package kopach
 
 import (
+	"github.com/davecgh/go-spew/spew"
+	"github.com/p9c/pod/pkg/broadcast"
 	"github.com/p9c/pod/pkg/log"
+	"net"
 	"sync"
 
 	"github.com/p9c/pod/pkg/conte"
@@ -13,12 +16,19 @@ func Main(cx *conte.Xt, quit chan struct{}, wg *sync.WaitGroup) {
 		log.WARN("starting kopach standalone miner worker")
 	out:
 		for {
+			cancel := broadcast.Listen(broadcast.DefaultAddress, msgHandler)
 			select {
 			case <-quit:
 				log.DEBUG("quitting on killswitch")
+				cancel()
 				break out
 			}
 		}
 		wg.Done()
 	}()
+}
+
+func msgHandler(src *net.UDPAddr, n int, b []byte) {
+	log.INFO(n, " bytes read from ", src)
+	spew.Dump(b[:n])
 }
