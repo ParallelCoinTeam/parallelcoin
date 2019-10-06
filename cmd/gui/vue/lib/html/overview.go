1bc1af683fe2d938710a95883b711134239bdb70
marcetin <marcetin@gmail.com>
2019-10-06 15:00:15 +0200

    michel drolja
    Former-commit-id: 1144e1b13abf2ffba47960d84e064686f1b4b233

--- cmd/gui/vue/lib/html/overview.go
+++ cmd/gui/vue/lib/html/overview.go
@@ -2,5 +2,5 @@ package html
 
 func VUEoverview() string {
 	// language=HTML
-	return `<div id='panelwalletstatus' class='Balance'></div><div class='Address'>asss</div><div class='Amount'></div><div class='NetHR'></div><div class='LocalHR'></div><div class='BottomLeft'><div class='b1'></div><div class='b2'></div><div class='b3'></div><div class='b4'></div><div class='b5'></div><div class='b6'></div></div><div class='Bottom'></div><div class='Log'></div><div id="panelstatus" class='Status'></div><div class='Mid'></div><div class='Txs'></div>`
+	return `<div id='panelwalletstatus'></div><div class='Address'>asss</div><div class='Amount'></div><div class='NetHR'></div><div class='LocalHR'></div><div class='BottomLeft'><div class='b1'></div><div class='b2'></div><div class='b3'></div><div class='b4'></div><div class='b5'></div><div class='b6'></div></div><div class='Bottom'></div><div class='Log'></div><div id="panelstatus"></div><div class='Mid'></div><div class='Txs'></div>`
 }
