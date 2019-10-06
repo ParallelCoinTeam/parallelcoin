069f6b7d882224ccde7faea6d1b589d670e3c920
marcetin <marcetin@gmail.com>
2019-10-06 10:33:18 +0200

    all doo

--- cmd/gui/vue/lib/html/overview.go
+++ cmd/gui/vue/lib/html/overview.go
@@ -1,5 +1,6 @@
 package html
 
 func VUEoverview() string {
-	return `<div id='balance' class='Balance'></div><div class='Address'></div><div class='Amount'></div><div class='NetHR'></div><div class='LocalHR'></div><div class='BottomLeft'><div class='b1'></div><div class='b2'></div><div class='b3'></div><div class='b4'></div><div class='b5'></div><div class='b6'></div></div><div class='Bottom'></div><div class='Log'></div><div class='Status'></div><div class='Mid'></div><div class='Txs'></div>`
+	// language=HTML
+	return `<div id='balance' class='Balance'>` + VUEbalance() + `</div><div class='Address'>asss</div><div class='Amount'></div><div class='NetHR'></div><div class='LocalHR'></div><div class='BottomLeft'><div class='b1'></div><div class='b2'></div><div class='b3'></div><div class='b4'></div><div class='b5'></div><div class='b6'></div></div><div class='Bottom'></div><div class='Log'></div><div class='Status'></div><div class='Mid'></div><div class='Txs'></div>`
 }
