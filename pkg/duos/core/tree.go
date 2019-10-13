f19d61b8f4767ba4f60ff45f533543102f55737d
marcetin <marcetin@gmail.com>
2019-10-13 17:05:13 +0200

    Gui Svelte

--- pkg/duos/core/tree.go
+++ pkg/duos/core/tree.go
@@ -1,19 +1,21 @@
 package core
 
 import (
-	"github.com/p9c/lorca"
+	"github.com/p9c/gui"
 	"github.com/p9c/pod/pkg/conf"
 	"github.com/p9c/pod/pkg/conte"
 	"github.com/p9c/pod/pkg/duos/db"
 	"github.com/p9c/pod/pkg/duos/srv"
 	"github.com/robfig/cron"
+	"sync"
 )
 
 // Core
 type DuOS struct {
+	sync.Mutex
 	CtX *conte.Xt        `json:"context"`
 	CrN *cron.Cron       `json:"cron"`
-	GuI lorca.UI         `json:"__OLDgui"`
+	GuI gui.UI           `json:"gui"`
 	DbS db.DuOSdb        `json:"database"`
 	CgG *conf.DuOSconfig `json:"configuration"`
 	SrV srv.DuOSservices `json:"services"`
