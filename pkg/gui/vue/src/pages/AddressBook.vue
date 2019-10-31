1cb375fb98a04abe6903c0bb678a0d53d5a081b4
marcetin <marcetin@gmail.com>
2019-10-31 17:23:04 +0100

    clean start

---
+++ pkg/gui/vue/src/pages/AddressBook.vue
@@ -0,0 +1,19 @@
+<template>
+    <main class="pageTransaction">
+        <PanelAddressBook :addressbook="duoSystem.addressBook"/>
+    </main>
+</template>
+
+<script>
+import { duoSystem } from '../DuOSyS.js'
+import PanelAddressBook from '../panels/PanelAddressBook.vue'
+
+export default {
+  data () { return { 
+	duoSystem }},
+  name: 'AddressBook',
+  components: {
+    PanelAddressBook
+  }
+}
+</script>
