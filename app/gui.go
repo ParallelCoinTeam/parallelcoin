3eafc4af2132f88ddff0387e813e4c20ce8a162b
Loki Verloren <l0k18@protonmail.com>
2019-10-31 14:17:38 +0100

    cleaning
    Former-commit-id: cb4cf378a93e578def25259bc0dce820b7da5ee8

---
+++ app/gui.go
@@ -0,0 +1,15 @@
+package app
+
+import (
+	"github.com/urfave/cli"
+
+	"github.com/p9c/pod/pkg/conte"
+)
+
+var guiHandle = func(cx *conte.Xt) func(c *cli.Context) error {
+	return func(c *cli.Context) error {
+		//L.Info("GUI was disabled for this build (server only version)")
+		//os.Exit(1)
+		return nil
+	}
+}
