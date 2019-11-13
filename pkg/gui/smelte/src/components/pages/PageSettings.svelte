06ff31fb61701900cf96b587bb5088ea56cafab9
marcetin <marcetin@gmail.com>
2019-11-13 06:08:29 +0100

    GUI QT Smelte da

---
+++ pkg/gui/smelte/src/components/pages/PageSettings.svelte
@@ -0,0 +1,14 @@
+<script>
+    import PanelSettings from "../panels/PanelSettings.svelte";
+</script>
+<style>
+
+</style>
+
+
+<svelte:head>
+	<title>Settings</title>
+</svelte:head>
+
+<PanelSettings />
+
