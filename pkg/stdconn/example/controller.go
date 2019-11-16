3bc26736d3bf5af6f7390c382122c3c9c441b1ff
Loki Verloren <l0k18@protonmail.com>
2019-11-16 23:09:40 +0100

    StdConn - RPC over stdio, with documentation
    Former-commit-id: eff5ede03a864184e7be28f1f7a5c5372e2fc863

--- pkg/stdconn/example/controller.go
+++ pkg/stdconn/example/controller.go
@@ -2,48 +2,17 @@ package main
 
 import (
 	"github.com/p9c/pod/pkg/log"
-	"github.com/p9c/pod/pkg/worker"
-	"io"
-	"net/rpc"
+	"github.com/p9c/pod/pkg/stdconn/example/hello/hello"
+	"github.com/p9c/pod/pkg/stdconn/worker"
 )
 
 func main() {
 	log.L.SetLevel("trace", true)
 	log.INFO("starting up example controller")
-	cmd := worker.SpawnWorker("go", "run", "worker.go")
-	client := NewHelloClient(cmd.StdConn)
+	cmd := worker.Spawn("go", "run", "hello/worker.go")
+	client := hello.NewClient(cmd.StdConn)
 	log.INFO("calling Hello.Say with 'worker'")
 	log.INFO("reply:", client.Say("worker"))
 	log.INFO("calling Hello.Bye")
 	log.INFO("reply:", client.Bye())
-	if err := cmd.KillWorker(); err != nil {
-		log.ERROR(err)
-	}
-}
-
-type HelloClient struct {
-	*rpc.Client
-}
-
-func NewHelloClient(conn io.ReadWriteCloser) *HelloClient {
-	return &HelloClient{rpc.NewClient(conn)}
-
-}
-
-func (h *HelloClient) Say(name string) (reply string) {
-	err := h.Call("Hello.Say", "worker", &reply)
-	if err != nil {
-		log.ERROR(err)
-		return "error: " + err.Error()
-	}
-	return
-}
-
-func (h *HelloClient) Bye() (reply string) {
-	err := h.Call("Hello.Bye", 1, &reply)
-	if err != nil {
-		log.ERROR(err)
-		return "error: " + err.Error()
-	}
-	return
 }
