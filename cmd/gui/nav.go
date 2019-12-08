2ce57e0dcf7cc640e5cbbedfafad7c3ad06e4508
marcetin <marcetin@gmail.com>
2019-12-08 22:37:49 +0100

    Gio based Duo UI
    Former-commit-id: 2c1d4cfea5c2ae8bdf1514d32b11ed4b0de55bad

--- cmd/gui/nav.go
+++ cmd/gui/nav.go
@@ -1,37 +1,33 @@
 package gui
 
-import (
-	"github.com/p9c/pod/pkg/conte"
-	"github.com/p9c/pod/pkg/log"
-)
 
-type DuOSnav struct {
-	cx     *conte.Xt
-	Screen string `json:"screen"`
-	Config DuOSsettings `json:"config"`
-	History DuOShistory `json:"history"`
-}
-
-func
-(nav *DuOSnav) GetScreen(s string) {
-	nav.Screen = s
-	log.INFO("NAV:VAR->", s)
-	switch s {
-	case "PageOverview":
-		log.INFO("NAV:VAR->", s)
-	case "PageHistory":
-		log.INFO("NAV:VAR->", s)
-		nav.History.cx = nav.cx
-	nav.History.GetDuOShistory()
-		log.INFO("NAV:HISTORY:VAR->", nav.History.txs)
-	case "PageAddressBook":
-		log.INFO("NAV:VAR->", s)
-	case "PageExplorer":
-		log.INFO("NAV:VAR->", s)
-	case "PageSettings":
-		log.INFO("NAV:VAR->", s)
-		nav.Config.cx = nav.cx
-		nav.Config.GetCoreSettings()
-		log.INFO("NAV:SETTINGS:VAR->", nav.Config.Daemon.Config)
-	}
-}
+//type DuOSnav struct {
+//	cx     *conte.Xt
+//	Screen string `json:"screen"`
+//	Config DuOSsettings `json:"config"`
+//	History DuOShistory `json:"history"`
+//}
+//
+//func
+//(nav *DuOSnav) GetScreen(s string) {
+//	nav.Screen = s
+//	log.INFO("NAV:VAR->", s)
+//	switch s {
+//	case "PageOverview":
+//		log.INFO("NAV:VAR->", s)
+//	case "PageHistory":
+//		log.INFO("NAV:VAR->", s)
+//		nav.History.cx = nav.cx
+//	nav.History.GetDuOShistory()
+//		log.INFO("NAV:HISTORY:VAR->", nav.History.txs)
+//	case "PageAddressBook":
+//		log.INFO("NAV:VAR->", s)
+//	case "PageExplorer":
+//		log.INFO("NAV:VAR->", s)
+//	case "PageSettings":
+//		log.INFO("NAV:VAR->", s)
+//		nav.Config.cx = nav.cx
+//		nav.Config.GetCoreSettings()
+//		log.INFO("NAV:SETTINGS:VAR->", nav.Config.Daemon.Config)
+//	}
+//}
