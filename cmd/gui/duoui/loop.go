d886c82f6901e4756baa01d9dd416bfe60654a3c
Loki Verloren <l0k18@protonmail.com>
2019-12-23 18:15:49 +0100

    gui kills backend, backend kills gui, GUI has pre-ready frameevent loop

--- cmd/gui/duoui/loop.go
+++ cmd/gui/duoui/loop.go
@@ -28,6 +28,30 @@ func DuoUImainLoop(duo *models.DuoUI, cx *conte.Xt, rc *rcd.RcVar) error {
 			e.Frame(duo.Gc.Ops)
 		}
 	}
+	for {
+		select {
+		case <-duo.Quit:
+			interrupt.Request()
+			// This case is for handling when some external application is controlling the GUI and to gracefully
+			// handle the back-end servers being shut down by the interrupt library receiving an interrupt signal
+			// Probably nothing needs to be run between starting it and shutting down
+			<-interrupt.HandlersDone
+			log.DEBUG("closing GUI from interrupt/quit signal")
+			return errors.New("shutdown triggered from back end")
+		case e := <-duo.ww.Events():
+			switch e := e.(type) {
+			case system.DestroyEvent:
+				interrupt.Request()
+				// Here do cleanup like are you sure (optional) modal or shutting down indefinite spinner
+				<-interrupt.HandlersDone
+				return e.Err
+			case system.FrameEvent:
+				duo.gc.Reset(e.Config, e.Size)
+				DuoUIgrid(duo)
+				e.Frame(duo.gc.Ops)
+			}
+		}
+	}
 }
 
 // START OMIT
