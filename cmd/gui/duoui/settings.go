4f08a06d88cf01c7ce4868795c97ad45e6e38e86
marcetin <marcetin@gmail.com>
2019-12-26 17:30:51 +0100

    Settings page tabs done, going on fields

--- cmd/gui/duoui/settings.go
+++ cmd/gui/duoui/settings.go
@@ -11,14 +11,17 @@ import (
 	"github.com/p9c/pod/pkg/log"
 )
 
-func DuoUIsettings(duo *models.DuoUI, cx *conte.Xt, rc *rcd.RcVar) {
-	groupsList := &layout.List{
+var (
+	groupsList = &layout.List{
 		Axis:      layout.Horizontal,
 		Alignment: layout.Middle,
 	}
-	fieldsList := &layout.List{
+	fieldsList = &layout.List{
 		Axis: layout.Vertical,
 	}
+)
+
+func DuoUIsettings(duo *models.DuoUI, cx *conte.Xt, rc *rcd.RcVar) {
 
 	layout.Flex{
 		Axis: layout.Vertical,
@@ -35,30 +38,21 @@ func DuoUIsettings(duo *models.DuoUI, cx *conte.Xt, rc *rcd.RcVar) {
 
 						duo.Th.H3(duo.Conf.Tabs.Current).Layout(duo.Gc)
 					}),
-
 					layout.Rigid(func() {
 						cs := duo.Gc.Constraints
 						helpers.DuoUIdrawRectangle(duo.Gc, cs.Width.Max, 64, helpers.HexARGB("ffcfcfcf"), [4]float32{0, 0, 0, 0}, unit.Dp(0))
-
 						groupsNumber := len(rc.Settings.Daemon.Schema.Groups)
-
 						groupsList.Layout(duo.Gc, groupsNumber, func(i int) {
-
 							in.Layout(duo.Gc, func() {
 								i = groupsNumber - 1 - i
 								t := rc.Settings.Daemon.Schema.Groups[i]
 								txt := fmt.Sprint(t.Legend)
-
 								for duo.Conf.Tabs.TabsList[txt].Clicked(duo.Gc) {
 									duo.Conf.Tabs.Current = txt
 									log.INFO(txt)
 								}
-								//th.Button("Click me!").Layout(gtx, button)
-
 								duo.Th.Button(txt).Layout(duo.Gc, duo.Conf.Tabs.TabsList[txt])
-
 							})
-
 						})
 					}))
 
@@ -66,16 +60,25 @@ func DuoUIsettings(duo *models.DuoUI, cx *conte.Xt, rc *rcd.RcVar) {
 		}),
 		layout.Flexed(1, func() {
 			//cs := duo.Gc.Constraints
-
 			for _, fields := range rc.Settings.Daemon.Schema.Groups {
-
 				if fmt.Sprint(fields.Legend) == duo.Conf.Tabs.Current {
-
 					fieldsList.Layout(duo.Gc, len(fields.Fields), func(il int) {
 						il = len(fields.Fields) - 1 - il
 						tl := fields.Fields[il]
-						txtc := fmt.Sprint(tl.Name)
-						duo.Th.H6(txtc).Layout(duo.Gc)
+						layout.Flex{
+							Axis: layout.Vertical,
+						}.Layout(duo.Gc,
+							layout.Rigid(func() {
+								in.Layout(duo.Gc, func() {
+									duo.Th.H6(fmt.Sprint(tl.Name)).Layout(duo.Gc)
+								})
+							}),
+							layout.Rigid(func() {
+								in.Layout(duo.Gc, func() {
+									duo.Th.Body2(fmt.Sprint(tl.Description)).Layout(duo.Gc)
+								})
+							}),
+						)
 					})
 				}
 			}
