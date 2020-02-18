38df6faf46d3048b4d8b789709f2cb3e39143872
marcetin <marcetin@gmail.com>
2020-02-18 16:57:46 +0100

    Loader - First run, create wallet database

--- cmd/gui/duoui/wallet.go
+++ cmd/gui/duoui/wallet.go
@@ -7,7 +7,6 @@ import (
 	"github.com/p9c/pod/pkg/gui/text"
 	"github.com/p9c/pod/pkg/gui/unit"
 	"github.com/p9c/pod/pkg/log"
-	"image/color"
 )
 
 var (
@@ -15,11 +14,11 @@ var (
 	confirmPassPhrase string
 	passEditor        = &controller.Editor{
 		SingleLine: true,
-		Submit:     true,
+		//Submit:     true,
 	}
 	confirmPassEditor = &controller.Editor{
 		SingleLine: true,
-		Submit:     true,
+		//Submit:     true,
 	}
 
 	encryption         = new(controller.CheckBox)
@@ -31,34 +30,35 @@ func (ui *DuoUI) DuoUIloaderCreateWallet() {
 	cs := ui.ly.Context.Constraints
 	theme.DuoUIdrawRectangle(ui.ly.Context, cs.Width.Max, cs.Height.Max, ui.ly.Theme.Color.Bg, [4]float32{0, 0, 0, 0}, [4]float32{0, 0, 0, 0})
 	layout.Center.Layout(ui.ly.Context, func() {
-
 		controllers := []func(){
 			func() {
-				bale := ui.ly.Theme.H3(passPhrase)
+				bale := ui.ly.Theme.Body1(passPhrase)
 				bale.Font.Typeface = ui.ly.Theme.Font.Primary
 				bale.Color = theme.HexARGB(ui.ly.Theme.Color.Dark)
 				bale.Layout(ui.ly.Context)
 			},
 			func() {
-				balr := ui.ly.Theme.H3(confirmPassPhrase)
+				balr := ui.ly.Theme.Body1(confirmPassPhrase)
 				balr.Font.Typeface = ui.ly.Theme.Font.Primary
 				balr.Color = theme.HexARGB(ui.ly.Theme.Color.Dark)
 				balr.Layout(ui.ly.Context)
 			},
 			func() {
-				bal := ui.ly.Theme.H3("Enter the private passphrase for your new wallet:")
+				bal := ui.ly.Theme.H5("Enter the private passphrase for your new wallet:")
 				bal.Font.Typeface = ui.ly.Theme.Font.Primary
 				bal.Color = theme.HexARGB(ui.ly.Theme.Color.Dark)
 				bal.Layout(ui.ly.Context)
+			},
+			func() {
 				layout.UniformInset(unit.Dp(8)).Layout(ui.ly.Context, func() {
 					e := ui.ly.Theme.DuoUIeditor("Enter Passpharse")
 					e.Font.Typeface = ui.ly.Theme.Font.Primary
 					e.Font.Style = text.Regular
 					e.Layout(ui.ly.Context, passEditor)
 					for _, e := range passEditor.Events(ui.ly.Context) {
-						if e, ok := e.(controller.SubmitEvent); ok {
-							passPhrase = e.Text
-							passEditor.SetText("")
+						switch e.(type) {
+						case controller.ChangeEvent:
+							passPhrase = passEditor.Text()
 						}
 					}
 				})
@@ -70,9 +70,9 @@ func (ui *DuoUI) DuoUIloaderCreateWallet() {
 					e.Font.Style = text.Regular
 					e.Layout(ui.ly.Context, confirmPassEditor)
 					for _, e := range confirmPassEditor.Events(ui.ly.Context) {
-						if e, ok := e.(controller.SubmitEvent); ok {
-							confirmPassPhrase = e.Text
-							confirmPassEditor.SetText("")
+						switch e.(type) {
+						case controller.ChangeEvent:
+							confirmPassPhrase = confirmPassEditor.Text()
 						}
 					}
 				})
@@ -80,13 +80,13 @@ func (ui *DuoUI) DuoUIloaderCreateWallet() {
 			func() {
 				encryptionCheckBox := ui.ly.Theme.DuoUIcheckBox("Do you want to add an additional layer of encryption for public data?")
 				encryptionCheckBox.Font.Typeface = ui.ly.Theme.Font.Primary
-				encryptionCheckBox.Color = color.RGBA{A: 0xff, R: 0xcf, G: 0xcf, B: 0xcf}
+				encryptionCheckBox.Color = theme.HexARGB(ui.ly.Theme.Color.Dark)
 				encryptionCheckBox.Layout(ui.ly.Context, encryption)
 			},
 			func() {
 				seedCheckBox := ui.ly.Theme.DuoUIcheckBox("Do you have an existing wallet seed you want to use?")
 				seedCheckBox.Font.Typeface = ui.ly.Theme.Font.Primary
-				seedCheckBox.Color = color.RGBA{A: 0xff, R: 0xcf, G: 0xcf, B: 0xcf}
+				seedCheckBox.Color = theme.HexARGB(ui.ly.Theme.Color.Dark)
 				seedCheckBox.Layout(ui.ly.Context, seed)
 			},
 			func() {
@@ -96,7 +96,6 @@ func (ui *DuoUI) DuoUIloaderCreateWallet() {
 					if passPhrase != "" && passPhrase == confirmPassPhrase {
 						ui.rc.CreateWallet(passPhrase, "", "", "")
 						log.INFO("WOIKOS!")
-
 					}
 					log.INFO("confirmPassPhrase: ", confirmPassPhrase)
 					log.INFO("passPhrase: ", passPhrase)
