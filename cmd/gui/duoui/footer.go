862d4af4c8ed8898546f1478cc3ebadb0d04f359
marcetin <marcetin@gmail.com>
2020-03-22 02:48:04 +0100

    Footer fix

--- cmd/gui/duoui/footer.go
+++ cmd/gui/duoui/footer.go
@@ -1,10 +1,10 @@
 package duoui
 
 import (
+	"fmt"
 	"gioui.org/layout"
 
 	"github.com/p9c/pod/cmd/gui/component"
-	"github.com/p9c/pod/pkg/gelook"
 )
 
 var (
@@ -15,11 +15,16 @@ var (
 
 func (ui *DuoUI) DuoUIfooter() func() {
 	return func() {
-		cs := ui.ly.Context.Constraints
-		gelook.DuoUIdrawRectangle(ui.ly.Context, cs.Width.Max, 64, ui.ly.Theme.Colors["Dark"], [4]float32{0, 0, 0, 0}, [4]float32{0, 0, 0, 0})
-		layout.Flex{Spacing: layout.SpaceBetween}.Layout(ui.ly.Context,
-			layout.Rigid(component.FooterLeftMenu(ui.rc, ui.ly.Context, ui.ly.Theme, ui.ly.Pages)),
-			layout.Rigid(component.FooterRightMenu(ui.rc, ui.ly.Context, ui.ly.Theme, ui.ly.Pages)),
-		)
+		footer := ui.ly.Theme.DuoUIitem(0, ui.ly.Theme.Colors["Dark"])
+		footer.FullWidth = true
+		footer.Layout(ui.ly.Context, layout.N, func() {
+			fmt.Println("footer")
+			fmt.Println(ui.ly.Context.Constraints.Width.Max)
+			layout.Flex{Spacing: layout.SpaceBetween}.Layout(ui.ly.Context,
+				layout.Rigid(component.FooterLeftMenu(ui.rc, ui.ly.Context, ui.ly.Theme, ui.ly.Pages)),
+				layout.Flexed(1, func() {}),
+				layout.Rigid(component.FooterRightMenu(ui.rc, ui.ly.Context, ui.ly.Theme, ui.ly.Pages)),
+			)
+		})
 	}
 }
--- pkg/gelook/item.go
+++ pkg/gelook/item.go
@@ -23,6 +23,7 @@ type DuoUIitem struct {
 	BgColor       string
 	TxColorHover  string
 	BgColorHover  string
+	FullWidth     bool
 	Width         int
 	Height        int
 	CornerRadius  unit.Value
@@ -54,6 +55,9 @@ func (t *DuoUItheme) DuoUIitem(padding int, background string) DuoUIitem {
 func (d DuoUIitem) Layout(gtx *layout.Context, direction layout.Direction, itemContent func()) {
 	hmin := gtx.Constraints.Width.Min
 	vmin := gtx.Constraints.Height.Min
+	if d.FullWidth {
+		hmin = gtx.Constraints.Width.Max
+	}
 	layout.Stack{Alignment: layout.W}.Layout(gtx,
 		layout.Expanded(func() {
 			rr := float32(gtx.Px(unit.Dp(0)))
