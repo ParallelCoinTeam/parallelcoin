94452f388cb014cb3ef4979e7d8cb0c27841a218
marcetin <marcetin@gmail.com>
2020-03-22 02:50:29 +0100

    Footer fix

--- cmd/gui/duoui/footer.go
+++ cmd/gui/duoui/footer.go
@@ -1,7 +1,6 @@
 package duoui
 
 import (
-	"fmt"
 	"gioui.org/layout"
 
 	"github.com/p9c/pod/cmd/gui/component"
@@ -18,8 +17,6 @@ func (ui *DuoUI) DuoUIfooter() func() {
 		footer := ui.ly.Theme.DuoUIitem(0, ui.ly.Theme.Colors["Dark"])
 		footer.FullWidth = true
 		footer.Layout(ui.ly.Context, layout.N, func() {
-			fmt.Println("footer")
-			fmt.Println(ui.ly.Context.Constraints.Width.Max)
 			layout.Flex{Spacing: layout.SpaceBetween}.Layout(ui.ly.Context,
 				layout.Rigid(component.FooterLeftMenu(ui.rc, ui.ly.Context, ui.ly.Theme, ui.ly.Pages)),
 				layout.Flexed(1, func() {}),
