73fe567a96658721ffd7a07e6baf9ccb57b66d54
marcetin <marcetin@gmail.com>
2020-03-26 18:17:57 +0100

    Button fix

--- cmd/monitor/helpers.go
+++ cmd/monitor/helpers.go
@@ -44,7 +44,7 @@ func (s *State) IconButton(icon, fg, bg string, button *gel.Button) {
 	s.Theme.DuoUIbutton("", "", "",
 		s.Theme.Colors[bg], "", s.Theme.Colors[fg], icon,
 		s.Theme.Colors[fg], 0, 32, 41, 41,
-		8, 8, 8, 8).IconLayout(s.Gtx, button)
+		0, 0, 0, 0).IconLayout(s.Gtx, button)
 }
 
 func (s *State) TextButton(label, fontFace string, fontSize int, fg, bg string,
@@ -58,7 +58,7 @@ func (s *State) TextButton(label, fontFace string, fontSize int, fg, bg string,
 		s.Theme.Colors[fg],
 		"settingsIcon",
 		s.Theme.Colors["Light"],
-		fontSize, 0, 80, 32, 4, 4, 4, 4).
+		fontSize, 0, 80, 32, 11, 10, 9, 10).
 		Layout(s.Gtx, button)
 }
 
