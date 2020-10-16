48987b433d8aa2a69642e305767618c58bf27e28
marcetin <marcetin@gmail.com>
2020-10-16 22:37:53 +0200

    There is good chance I am going right way, using old funcs

---
+++ pkg/gui/wallet/dap/mod/events.go
@@ -0,0 +1,102 @@
+package mod
+
+import (
+	"fmt"
+	"time"
+
+	"go.uber.org/atomic"
+
+	blockchain "github.com/p9c/pod/pkg/chain"
+)
+
+const (
+	NewBlock uint32 = iota
+	// Add new events here
+	EventCount
+)
+
+type Event struct {
+	Type    uint32
+	Payload []byte
+}
+
+var EventsChan = make(chan Event, 1)
+
+func (r *RcVar) ListenInit(trigger chan struct{}) {
+	//L.Debug("listeninit")
+	r.Events = EventsChan
+	r.UpdateTrigger = trigger
+
+	// first time starting up get all of these and trigger update
+	update(r)
+	//r.labelMiningAddreses()
+
+	var ready atomic.Bool
+	ready.Store(false)
+	r.Cx.RealNode.Chain.Subscribe(func(callback *blockchain.Notification) {
+		switch callback.Type {
+		case blockchain.NTBlockAccepted,
+			blockchain.NTBlockConnected,
+			blockchain.NTBlockDisconnected:
+			if !ready.Load() {
+				return
+			}
+			update(r)
+			// go r.toastAdd("New block: "+fmt.Sprint(callback.Data.(*util.Block).Height()), callback.Data.(*util.Block).Hash().String())
+		}
+	})
+	go func() {
+		ticker := time.NewTicker(time.Second)
+	out:
+		for {
+			select {
+			case <-ticker.C:
+				if !ready.Load() {
+					if r.Cx.IsCurrent() {
+						ready.Store(true)
+						// 		go func() {
+						// 			r.cx.WalletServer.Rescan(nil, nil)
+						// 			r.Ready <- struct{}{}
+						// 			r.UpdateTrigger <- struct{}{}
+						// 		}()
+					}
+				}
+				//r.GetDuoUIconnectionCount()
+				r.UpdateTrigger <- struct{}{}
+			// L.Warn("GetDuoUIconnectionCount")
+			case <-r.Cx.WalletServer.Update:
+				update(r)
+			case <-r.Cx.KillAll:
+				break out
+			}
+		}
+	}()
+	//L.Warn("event update listener started")
+	return
+}
+
+func update(r *RcVar) {
+	fmt.Println("kakakak")
+	// L.Warn("GetDuoUIbalance")
+	//r.GetDuoUIbalance()
+	// L.Warn("GetDuoUIunconfirmedBalance")
+	//r.GetDuoUIunconfirmedBalance()
+	// L.Warn("GetDuoUItransactionsNumber")
+	//r.GetDuoUItransactionsNumber()
+	// r.GetTransactions()
+	// L.Warn("GetLatestTransactions")
+	//r.GetLatestTransactions()
+	// L.Info("")
+	// L.Info("UPDATE")
+	// L.Trace(r.History.PerPage)
+	// L.Info("")
+	// r.GetDuoUIstatus()
+	// r.GetDuoUIlocalLost()
+	// r.GetDuoUIblockHeight()
+	// L.Warn("GetDuoUIblockCount")
+	//r.GetDuoUIdifficulty()
+	//r.GetDuoUIblockCount()
+	//r.GetPeerInfo()
+	// L.Warn("GetDuoUIdifficulty")
+	r.UpdateTrigger <- struct{}{}
+}
--- pkg/gui/wallet/dap/mod/mod.go
+++ pkg/gui/wallet/dap/mod/mod.go
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: Unlicense OR MIT
+// SPDX-License-Identifier: Unlicense OR MIT
 
 package mod
 
 import (
-	"context"
 	"gioui.org/app"
 	"gioui.org/layout"
 	"gioui.org/op"
@@ -14,14 +14,15 @@ import (
 
 //Duo App Platform
 type Dap struct {
+	Rc         *RcVar
 	BeforeMain map[int]func()
 	Main       func()
 	AfterMain  map[int]func()
-	Ctx        context.Context
-	Tik        map[int]func()
-	UI         *UserInterface
-	S          *Settings
-	Apps       map[string]Sap
+	//Ctx        context.Context
+	//Tik        map[int]func()
+	UI   *UserInterface
+	S    *Settings
+	Apps map[string]Sap
 }
 
 type Sap struct {
