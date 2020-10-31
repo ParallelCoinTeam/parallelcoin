6ab638762113c1afdcd0b33fe3b1f9406bd655dd
Loki <stalker.loki@protonmail.ch>
2020-10-31 11:21:08 +0200

    starting to improve overview page

--- cmd/gui/overview.go
+++ cmd/gui/overview.go
@@ -1,70 +1,148 @@
 package gui
 
 import (
-	"strconv"
+	"fmt"
+	"strings"
 
 	l "gioui.org/layout"
+	"gioui.org/op"
+
+	"github.com/p9c/pod/pkg/gui/p9"
 )
 
 func (wg *WalletGUI) OverviewPage() l.Widget {
 	return func(gtx l.Context) l.Dimensions {
-		return wg.th.Flex().
-			Flexed(0.5,
-				wg.panel("Balances", wg.balances()),
-			).
-			Flexed(0.5,
-				wg.panel("Recent transactions", wg.th.Body1("transactions").Color("PanelText").Fn),
-			).Fn(gtx)
+		return wg.th.Responsive(*wg.App.Size, p9.Widgets{
+			{
+				Widget: wg.th.VFlex().
+					Rigid(
+						wg.panel("Balances", true, wg.balances()),
+					).
+					Rigid(
+						wg.panel("Recent transactions", true, wg.th.Body1("transactions").Color("PanelText").Fn),
+					).Fn,
+			},
+			{
+				Size: 1280,
+				Widget: wg.th.Flex().
+					Rigid(
+						wg.panel("Balances", false, wg.balances()),
+					).
+					Flexed(1,
+						wg.panel("Recent transactions", true, wg.th.Body1("transactions").Color("PanelText").Fn),
+					).Fn},
+		}).Fn(gtx)
 	}
+}
+
+func leftPadTo(length, limit int, txt string) string {
+	if len(txt) > limit {
+		return txt[limit-len(txt):]
+	}
+	pad := length - len(txt)
+	return strings.Repeat(" ", pad) + txt
 }
 
 func (wg *WalletGUI) balances() l.Widget {
 	return func(gtx l.Context) l.Dimensions {
-		return wg.Inset(0.25,
-			wg.th.VFlex().
-				Rigid(
-					wg.row("Available:",
-						strconv.FormatFloat(wg.State.balance, 'f', -1, 64)),
-				).
-				Rigid(
-					wg.row("Pending:",
-						strconv.FormatFloat(wg.State.balanceUnconfirmed, 'f', -1, 64)),
-				).
-				Rigid(
-					wg.row("Total:",
-						strconv.FormatFloat(wg.State.balanceUnconfirmed+wg.State.balance, 'f', 8, 64)),
-				).Fn,
-		).Fn(gtx)
-	}
-}
-
-func (wg *WalletGUI) row(title, value string) l.Widget {
-	return func(gtx l.Context) l.Dimensions {
+		balance := leftPadTo(15, 15, fmt.Sprintf("%6.8f", wg.State.balance))
+		balanceUnconfirmed := leftPadTo(15, 15, fmt.Sprintf("%6.8f", wg.State.balanceUnconfirmed))
+		balanceTotal := leftPadTo(15, 15, fmt.Sprintf("%6.8f", wg.State.balanceUnconfirmed+wg.State.balance))
 		return wg.Inset(0.25,
 			wg.th.Flex().
-				SpaceBetween().
 				Rigid(
-					wg.Inset(0.0, wg.Inset(0.5, wg.Body1(title).Color("DocText").Fn).Fn).Fn,
-				).Flexed(1,
-				wg.Inset(0.0, wg.Inset(0.5, wg.Caption(value).Color("DocText").Fn).Fn).Fn,
-			).Fn,
+					wg.th.VFlex().
+						Rigid(
+							wg.th.Inset(0.25,
+								wg.th.Body1("Available:").Font("bariol bold").Fn,
+							).Fn,
+						).
+						Rigid(
+							wg.th.Inset(0.25,
+								wg.th.Body1("Pending:").Font("bariol bold").Fn,
+							).Fn,
+						).
+						Rigid(
+							wg.th.Inset(0.25,
+								wg.th.Body1("Total:").Font("bariol bold").Fn,
+							).Fn,
+						).
+						Fn,
+				).
+				Rigid(
+					wg.th.VFlex().
+						Rigid(
+							wg.th.Inset(0.25,
+								wg.th.Flex().AlignBaseline().
+									Rigid(wg.th.Body1(" ").Fn).
+									Rigid(
+										wg.th.Caption(balance).
+											Font("go regular").
+											Fn,
+									).
+									Fn,
+							).Fn,
+						).
+						Rigid(
+							wg.th.Inset(0.25,
+								wg.th.Flex().AlignBaseline().
+									Rigid(wg.th.Body1(" ").Fn).
+									Rigid(
+										wg.th.Caption(balanceUnconfirmed).
+											Font("go regular").
+											Fn,
+									).Fn,
+							).Fn,
+						).
+						Rigid(
+							wg.th.Inset(0.25,
+								wg.th.Flex().AlignBaseline().
+									Rigid(wg.th.Body1(" ").Fn).
+									Rigid(
+										wg.th.Caption(balanceTotal).
+											Font("go regular").
+											Fn,
+									).Fn,
+							).Fn,
+						).Fn,
+				).Fn,
 		).Fn(gtx)
 	}
 }
 
-func (wg *WalletGUI) panel(title string, content l.Widget) l.Widget {
+func (wg *WalletGUI) panel(title string, fill bool, content l.Widget) l.Widget {
 	return func(gtx l.Context) l.Dimensions {
-		return wg.Inset(0.25,
+		w := wg.Inset(0.25,
 			wg.th.VFlex().
 				Rigid(
-					wg.Inset(0.0, wg.Fill("DocText", wg.Inset(0.5, wg.H6(title).Color("DocBg").Fn).Fn).Fn).Fn,
-				).Flexed(1,
-				wg.Fill("DocBg",
-					wg.Inset(0.25,
-						content,
+					wg.Fill("DocText",
+						wg.th.Flex().
+							Rigid(
+								wg.Inset(0.5,
+									wg.H6(title).Color("DocBg").Fn,
+								).Fn,
+							).Fn,
+					).Fn,
+				).
+				Rigid(
+					wg.Fill("DocBg",
+						wg.Inset(0.25,
+							content,
+						).Fn,
 					).Fn,
 				).Fn,
-			).Fn,
-		).Fn(gtx)
+		).Fn
+		if !fill {
+			// render the widgets onto a second context to get their dimensions
+			gtx1 := p9.CopyContextDimensions(gtx, gtx.Constraints.Max, l.Horizontal)
+			// generate the dimensions for all the list elements
+			child := op.Record(gtx1.Ops)
+			d := w(gtx1)
+			_ = child.Stop()
+			gtx.Constraints.Max.X = d.Size.X
+			gtx.Constraints.Max.Y = d.Size.Y
+			gtx.Constraints.Min = gtx.Constraints.Max
+		}
+		return w(gtx)
 	}
 }
