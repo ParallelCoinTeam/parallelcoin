69f79f519639ff3a522c1742fc5447850e4166c6
marcetin <marcetin@gmail.com>
2020-11-10 17:54:45 +0100

    Merge remote-tracking branch 'origin/refgui' into refgui

---
+++ cmd/gui/console.go_
@@ -0,0 +1,176 @@
+package gui
+
+import (
+	"encoding/json"
+	"fmt"
+	"sort"
+	"strings"
+	"time"
+
+	l "gioui.org/layout"
+
+	"github.com/p9c/pod/pkg/rpc/btcjson"
+	"github.com/p9c/pod/pkg/rpc/chainrpc"
+	"github.com/p9c/pod/pkg/rpc/legacy"
+)
+
+type Console struct {
+	Commands       []ConsoleCommand
+	CommandsNumber int
+}
+type ConsoleCommand struct {
+	Com      interface{}
+	ComID    string
+	Category string
+	Out      string
+	Time     time.Time
+}
+
+type ConsoleCommandsNumber struct {
+	CommandsNumber int
+}
+
+// var (
+//	consoleInputField = wg.th.Input("", "Amount", "Primary", "DocText", 25, func(pass string) {})
+//	consoleOutputList = &layout.List{
+//		Axis:        layout.Vertical,
+//		ScrollToEnd: true,
+//	}
+// )
+
+func (wg *WalletGUI) ConsolePage() l.Widget {
+	return wg.th.VFlex().
+		Flexed(1,
+			wg.Inset(0.25,
+				func(gtx l.Context) l.Dimensions {
+					return wg.lists["console"].Vertical().Length(len(wg.sendAddresses)).ListElement(wg.consoleRow).Fn(gtx)
+				},
+			).Fn,
+		).
+		Rigid(
+			wg.consoleInput(),
+		).Fn
+}
+
+func (wg *WalletGUI) consoleRow(gtx l.Context, i int) l.Dimensions {
+	t := wg.console.Commands[i]
+	return wg.Flex().Vertical().AlignEnd().
+		Rigid(
+			wg.th.Caption("ds://" + t.ComID).
+				Font("go regular").
+				Color("PanelText").
+				TextScale(0.66).Fn,
+		).
+		Rigid(
+			wg.th.Caption(t.Out).
+				Font("go regular").
+				Color("PanelText").
+				TextScale(0.66).Fn,
+		).Fn(gtx)
+}
+
+func (wg *WalletGUI) consoleInput() l.Widget {
+	return wg.inputs["console"].Input("", "Run command", "Secondary", "Primary", 25, func(pass string) {
+		// func(e gel.SubmitEvent) {
+		wg.console.Commands = append(
+			wg.console.Commands,
+			ConsoleCommand{
+				ComID: wg.inputs["console"].GetText(),
+				Time:  time.Time{},
+				Out:   wg.ConsoleCmd(wg.inputs["console"].GetText()),
+			})
+	}).Fn
+}
+func (wg *WalletGUI) ConsoleCmd(com string) (o string) {
+	split := strings.Split(com, " ")
+	method := split[0]
+	args := split[1:]
+	var cmd, res interface{}
+	var err error
+	var errString, prev string
+	if method == "help" {
+		if len(args) < 1 {
+			method = ""
+			cmd = &btcjson.HelpCmd{Command: &method}
+			if res, err = chainrpc.RPCHandlers["help"].Fn(wg.cx.RPCServer, cmd, nil); Check(err) {
+				errString += fmt.Sprintln(err)
+			}
+			o += fmt.Sprintln(res)
+			if res, err = legacy.RPCHandlers["help"].
+				Handler(cmd, wg.cx.WalletServer, wg.cx.ChainClient); Check(err) {
+				errString += fmt.Sprintln(err)
+			}
+			o += fmt.Sprintln(res)
+			splitted := strings.Split(o, "\n")
+			sort.Strings(splitted)
+			var dedup []string
+			for i := range splitted {
+				if i > 0 {
+					if splitted[i] != prev {
+						dedup = append(dedup, splitted[i])
+					}
+				}
+				prev = splitted[i]
+			}
+			o = strings.Join(dedup, "\n")
+			if errString != "" {
+				o += "Error:\n"
+				o += errString
+			}
+		} else {
+			method = args[0]
+			// L.Debug("finding help for command", method)
+			if help, err := wg.cx.RPCServer.HelpCacher.RPCMethodHelp(
+				method); Check(err) {
+				o += err.Error() + "\n"
+				o += fmt.Sprintln(res)
+				cmd = &btcjson.HelpCmd{Command: &method}
+				if res, err = legacy.RPCHandlers["help"].
+					Handler(cmd, wg.cx.WalletServer, wg.cx.ChainClient); Check(err) {
+					errString += fmt.Sprintln(err)
+				}
+				o += fmt.Sprintln(res)
+			} else {
+				o += help
+			}
+			// if _, ok := legacy.RPCHandlers[method]; ok {
+			// 	o += "wallet server:\n"
+			// 	o += legacy.HelpDescsEnUS()[method]
+			// }
+			// if _, ok := rpc.RPCHandlers[method]; ok {
+			// 	o += "chain server:\n"
+			// 	o += rpc.HelpDescsEnUS[method]
+			// }
+		}
+		return
+	}
+	params := make([]interface{}, 0, len(split[1:]))
+	for _, arg := range args {
+		params = append(params, arg)
+	}
+	if cmd, err = btcjson.NewCmd(method, params...); Check(err) {
+		o += fmt.Sprintln(err)
+	}
+	if x, ok := chainrpc.RPCHandlers[method]; !ok {
+		if x, ok := legacy.RPCHandlers[method]; ok {
+			if res, err = x.Handler(cmd, wg.cx.WalletServer,
+				wg.cx.ChainClient); Check(err) {
+				o += err.Error()
+			}
+			// o += fmt.Sprintln(res)
+		}
+	} else {
+		if res, err = x.Fn(wg.cx.RPCServer, cmd, nil); Check(err) {
+			o += err.Error()
+		}
+		// o += fmt.Sprintln(res)
+	}
+	if res != nil {
+		if j, err := json.MarshalIndent(res, "",
+			"  "); !Check(err) {
+			o += string(j)
+		}
+	}
+	return
+
+}
---
+++ cmd/gui/createwallet.go
@@ -0,0 +1,219 @@
+package gui
+
+import (
+	"encoding/hex"
+	"math/rand"
+	"os"
+	"time"
+
+	l "gioui.org/layout"
+
+	"github.com/p9c/pod/pkg/chain/config/netparams"
+	"github.com/p9c/pod/pkg/chain/fork"
+	"github.com/p9c/pod/pkg/chain/mining/addresses"
+	"github.com/p9c/pod/pkg/util/hdkeychain"
+	"github.com/p9c/pod/pkg/wallet"
+)
+
+func (wg *WalletGUI) WalletPage(gtx l.Context) l.Dimensions {
+	return wg.th.
+		Fill("PanelBg",
+			wg.th.Flex().SpaceAround().AlignMiddle().
+				Rigid(wg.th.
+					VFlex().AlignMiddle().SpaceAround().
+					Rigid(
+						wg.th.VFlex().SpaceAround().AlignMiddle().
+							Rigid(
+								wg.th.H4("create new wallet").
+									Color("PanelText").
+									// Alignment(text.Middle).
+									Fn,
+							).
+							Rigid(
+								wg.th.Inset(0.25,
+									wg.passwords["passEditor"].Fn,
+								).Fn,
+							).
+							Rigid(
+								wg.th.Inset(0.25,
+									wg.passwords["confirmPassEditor"].Fn,
+								).Fn,
+							).
+							Rigid(
+								wg.th.Inset(0.25,
+									wg.inputs["walletSeed"].Fn,
+								).Fn,
+							).
+							Rigid(
+								wg.th.Inset(0.25,
+									wg.passwords["publicPassEditor"].Fn,
+								).Fn,
+							).
+							Rigid(wg.th.Inset(0.25,
+								func(gtx l.Context) l.Dimensions {
+									gtx.Constraints.Min.X = int(wg.th.TextSize.Scale(16).V)
+									return wg.CheckBox(wg.bools["testnet"].SetOnChange(func(b bool) {
+										Debug("testnet on?", b)
+									})).
+										IconColor("Primary").
+										TextColor("DocText").
+										Text("Use testnet?").
+										Fn(gtx)
+								},
+							).Fn,
+							).
+							Rigid(
+								wg.th.Body1("your seed").
+									Color("PanelText").
+									Fn,
+							).
+							Rigid(
+								func(gtx l.Context) l.Dimensions {
+									gtx.Constraints.Max.X = int(wg.TextSize.Scale(22).V)
+									return wg.th.Caption(wg.inputs["walletSeed"].GetText()).
+										Font("go regular").
+										TextScale(0.66).
+										Fn(gtx)
+								},
+							).
+							Rigid(
+								wg.th.Inset(0.5,
+									func(gtx l.Context) l.Dimensions {
+										gtx.Constraints.Max.X = int(wg.th.TextSize.Scale(36).V)
+										gtx.Constraints.Min.X = int(wg.th.TextSize.Scale(16).V)
+										return wg.CheckBox(wg.bools["ihaveread"].SetOnChange(func(b bool) {
+											Debug("confirmed read", b)
+										})).
+											IconColor("Primary").
+											TextColor("DocText").
+											Text("I have stored the seed and password safely " +
+												"and understand it cannot be recovered").
+											Fn(gtx)
+									},
+								).Fn,
+							).
+							Rigid(
+								func(gtx l.Context) l.Dimensions {
+									var b []byte
+									var err error
+									seedValid := true
+									if b, err = hex.DecodeString(wg.inputs["walletSeed"].GetText()); Check(err) {
+										seedValid = false
+									} else if len(b) != 0 && len(b) < hdkeychain.MinSeedBytes ||
+										len(b) > hdkeychain.MaxSeedBytes {
+										seedValid = false
+									}
+									if wg.passwords["passEditor"].GetPassword() == "" ||
+										wg.passwords["confirmPassEditor"].GetPassword() == "" ||
+										len(wg.passwords["passEditor"].GetPassword()) < 8 ||
+										wg.passwords["passEditor"].GetPassword() !=
+											wg.passwords["confirmPassEditor"].GetPassword() ||
+										!seedValid ||
+										!wg.bools["ihaveread"].GetValue() {
+										gtx = gtx.Disabled()
+									}
+									return wg.th.Flex().
+										Rigid(
+											wg.th.Button(wg.clickables["createWallet"]).
+												Background("Primary").
+												Color("Light").
+												SetClick(func() {
+													// go func() {
+													// wg.ShellRunCommandChan <- "stop"
+													Debug("clicked submit wallet")
+													if wg.bools["testnet"].GetValue() {
+														wg.cx.ActiveNet = &netparams.TestNet3Params
+														fork.IsTestnet = true
+													} else {
+														wg.cx.ActiveNet = &netparams.MainNetParams
+														fork.IsTestnet = false
+													}
+													*wg.cx.Config.WalletFile = *wg.cx.Config.DataDir +
+														string(os.PathSeparator) + wg.cx.ActiveNet.Name +
+														string(os.PathSeparator) + wallet.WalletDbName
+													dbDir := *wg.cx.Config.WalletFile
+													loader := wallet.NewLoader(wg.cx.ActiveNet, dbDir, 250)
+													seed, _ := hex.DecodeString(wg.inputs["walletSeed"].GetText())
+													w, err := loader.CreateNewWallet(
+														[]byte(wg.passwords["publicPassEditor"].GetPassword()),
+														[]byte(wg.passwords["passEditor"].GetPassword()),
+														seed,
+														time.Now(),
+														false,
+														wg.cx.Config,
+													)
+													if Check(err) {
+														panic(err)
+													}
+													Warn("refilling mining addresses")
+													addresses.RefillMiningAddresses(w, wg.cx.Config, wg.cx.StateCfg)
+													Warn("done refilling mining addresses")
+													w.Manager.Close()
+													w.Stop()
+													// Debug("starting up shell first time")
+													rand.Seed(time.Now().Unix())
+													// nodeport := rand.Intn(60000) + 1024
+													// walletport := rand.Intn(60000) + 1024
+													// *wg.cx.Config.RPCListeners = []string{fmt.Sprintf("127.0.0.1:%d", nodeport)}
+													// *wg.cx.Config.RPCConnect = fmt.Sprintf("127.0.0.1:%d", nodeport)
+													// *wg.cx.Config.WalletRPCListeners = []string{fmt.Sprintf("127.0.0.1:%d", walletport)}
+													// *wg.cx.Config.WalletServer = fmt.Sprintf("127.0.0.1:%d", walletport)
+													// *wg.cx.Config.ServerTLS = false
+													// *wg.cx.Config.TLS = false
+													// *wg.cx.Config.GenThreads = 1 // probably want it to be max ultimately
+													// wg.incdecs["generatethreads"].Current = 1
+													// *wg.cx.Config.Generate = true // probably don't want on ultimately
+													// save.Pod(wg.cx.Config)
+
+													// Debug("opening wallet")
+													// w, err = loader.OpenExistingWallet([]byte(*wg.cx.Config.WalletPass),
+													// 	false, wg.cx.Config)
+													// if err != nil {
+													// 	panic(err)
+													// }
+													// args := []string{os.Args[0], "-D", *wg.cx.Config.DataDir,
+													// 	"--pipelog", "wallet", "drophistory"}
+													// runner := exec.Command(args[0], args[1:]...)
+													// runner.Stderr = os.Stderr
+													// runner.Stdout = os.Stderr
+													// if err := runner.Start(); Check(err) {
+													// }
+													// time.Sleep(time.Second * 10)
+													// wg.ShellRunCommandChan <- "stop"
+													// wg.ShellRunCommandChan <- "run"
+													// wg.ShellRunCommandChan <- "stop"
+													// wg.ShellRunCommandChan <- "run"
+													// time.Sleep(time.Second * 10)
+													// time.Sleep(time.Second * 2)
+													// interrupt.RequestRestart()
+													// procAttr := new(os.ProcAttr)
+													// procAttr.Files = []*os.File{os.Stdin, os.Stdout, os.Stderr}
+													// os.StartProcess(os.Args[0], os.Args[1:], procAttr)
+													// *wg.App = *wg.GetAppWidget()
+													Debug("starting main app")
+													*wg.noWallet = false
+													wg.running = false
+													wg.mining = false
+													if err = wg.Runner(); Check(err) {
+													}
+													wg.ShellRunCommandChan <- "run"
+													wg.MinerRunCommandChan <- "run"
+													// Exec()
+													// }()
+												}).
+												CornerRadius(0).
+												Inset(0.5).
+												Text("create wallet").
+												Fn,
+										).
+										Fn(gtx)
+								},
+							).
+							Fn,
+					).
+					Fn,
+				).
+				Fn,
+		).
+		Fn(gtx)
+}
