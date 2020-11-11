cab687528bda46c2fab955fade9183c946994ad1
Loki <stalker.loki@protonmail.ch>
2020-11-11 11:51:37 +0200

    super basic working console

--- cmd/gui/console.go
+++ cmd/gui/console.go
@@ -1,8 +1,8 @@
 package gui
 
 import (
-	"fmt"
-	"os"
+	"bytes"
+	"encoding/json"
 	"regexp"
 	"strings"
 
@@ -13,6 +13,7 @@ import (
 
 	"github.com/p9c/pod/pkg/gui/p9"
 	"github.com/p9c/pod/pkg/rpc/btcjson"
+	"github.com/p9c/pod/pkg/rpc/ctl"
 )
 
 type Console struct {
@@ -43,7 +44,9 @@ func (wg *WalletGUI) ConsolePage() *Console {
 	c.editor.SetSubmit(func(txt string) {
 		Debug("submit", txt)
 		c.output = append(c.output,
-			wg.th.Body1(txt).Color("DocText").Fn)
+			func(gtx l.Context) l.Dimensions {
+				return wg.th.Body1(txt).Color("DocText").Font("bariol bold").Fn(gtx)
+			})
 		c.editor.SetText("")
 		split := strings.Split(txt, " ")
 		method, args := split[0], split[1:]
@@ -52,41 +55,21 @@ func (wg *WalletGUI) ConsolePage() *Console {
 			params = append(params, args[i])
 		}
 		Debug("method", method, "args", args)
-		if wg.WalletClient != nil {
-			// Ensure the specified method identifies a valid registered command and is one of the usable types.
-			usageFlags, err := btcjson.MethodUsageFlags(method)
-			if err != nil {
-				Error(err)
-				fmt.Fprintf(os.Stderr, "Unrecognized command '%s'\n", method)
-				// HelpPrint()
-				// os.Exit(1)
-			}
-			if usageFlags&unusableFlags != 0 {
-				fmt.Fprintf(os.Stderr, "The '%s' command can only be used via websockets\n", method)
-				// HelpPrint()
-				// os.Exit(1)
-			}
-			// // Attempt to create the appropriate command using the arguments provided by the user.
-			// cmd, err := btcjson.NewCmd(method, params...)
-			// if err != nil {
-			// 	Error(err)
-			// 	// Show the error along with its error code when it's a json. BTCJSONError as it realistically will always be
-			// 	// since the NewCmd function is only supposed to return errors of that type.
-			// 	if jerr, ok := err.(btcjson.BTCJSONError); ok {
-			// 		fmt.Fprintf(os.Stderr, "%s command: %v (code: %s)\n",
-			// 			method, err, jerr.ErrorCode)
-			// 		// CommandUsage(method)
-			// 		// os.Exit(1)
-			// 	}
-			// 	// The error is not a json.BTCJSONError and this really should not happen. Nevertheless fall back to just
-			// 	// showing the error if it should happen due to a bug in the package.
-			// 	fmt.Fprintf(os.Stderr, "%s command: %v\n", method, err)
-			// CommandUsage(method)
-			// os.Exit(1)
-			// }
-			// Marshal the command into a JSON-RPC byte slice in preparation for sending it to the RPC server.
-			// marshalledJSON, err = btcjson.MarshalCmd(1, cmd)
-
+		var err error
+		var result []byte
+		if result, err = ctl.Call(wg.cx, method, params...); Check(err) {
+		}
+		var buf bytes.Buffer
+		json.Indent(&buf, result, "", "  ")
+		out := buf.String()
+		Debug(out)
+		splitResult := strings.Split(out, "\n")
+		for i := range splitResult {
+			sri := splitResult[i]
+			c.output = append(c.output,
+				func(gtx l.Context) l.Dimensions {
+					return wg.th.Caption(sri).Color("DocText").Font("go regular").Fn(gtx)
+				})
 		}
 	})
 	clearClickableFn := func() {
