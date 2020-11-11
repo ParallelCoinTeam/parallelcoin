08789c8c5b9df67a252e73251d2c73be1f98fa80
Loki <stalker.loki@protonmail.ch>
2020-11-11 10:55:57 +0200

    amended to return nil and error on failure

--- pkg/rpc/ctl/ctl.go
+++ pkg/rpc/ctl/ctl.go
@@ -10,6 +10,23 @@ import (
 
 // Call uses settings in the context to call the method with the given parameters and returns the raw json bytes
 func Call(cx *conte.Xt, method string, params ...interface{}) (result []byte){
+	// Ensure the specified method identifies a valid registered command and is one of the usable types.
+	usageFlags, err := btcjson.MethodUsageFlags(method)
+	if err != nil {
+		Error(err)
+		fmt.Fprintf(os.Stderr, "Unrecognized command '%s'\n", method)
+		// HelpPrint()
+		// os.Exit(1)
+		return
+	}
+	if usageFlags&unusableFlags != 0 {
+		fmt.Fprintf(
+			os.Stderr,
+			"The '%s' command can only be used via websockets\n", method)
+		// HelpPrint()
+		// os.Exit(1)
+		return
+	}
 	// Attempt to create the appropriate command using the arguments provided by the user.
 	cmd, err := btcjson.NewCmd(method, params...)
 	if err != nil {
@@ -19,26 +36,30 @@ func Call(cx *conte.Xt, method string, params ...interface{}) (result []byte){
 		if jerr, ok := err.(btcjson.BTCJSONError); ok {
 			fmt.Fprintf(os.Stderr, "%s command: %v (code: %s)\n", method, err, jerr.ErrorCode)
 			// CommandUsage(method)
-			os.Exit(1)
+			// os.Exit(1)
+			return
 		}
 		// The error is not a json.BTCJSONError and this really should not happen. Nevertheless fall back to just
 		// showing the error if it should happen due to a bug in the package.
 		fmt.Fprintf(os.Stderr, "%s command: %v\n", method, err)
 		// CommandUsage(method)
-		os.Exit(1)
+		// os.Exit(1)
+		return
 	}
 	// Marshal the command into a JSON-RPC byte slice in preparation for sending it to the RPC server.
 	marshalledJSON, err := btcjson.MarshalCmd(1, cmd)
 	if err != nil {
 		Error(err)
-		fmt.Println(err)
-		os.Exit(1)
+		// fmt.Println(err)
+		// os.Exit(1)
+		return
 	}
 	// Send the JSON-RPC request to the server using the user-specified connection configuration.
 	result, err = sendPostRequest(marshalledJSON, cx)
 	if err != nil {
 		Error(err)
-		os.Exit(1)
+		// os.Exit(1)
+		return
 	}
 	return
 }
