9b529add1dc894ca643dcb89affb4ab345a0384e
Loki <stalker.loki@protonmail.ch>
2020-11-16 17:16:23 +0200

    attempting to fix linux text input double bug

--- gio/app/internal/xkb/xkb_unix.go
+++ gio/app/internal/xkb/xkb_unix.go
@@ -192,18 +192,20 @@ func (x *Context) DispatchKey(keyCode uint32, state key.State) (events []event.E
 		}
 	}
 	// Report only printable runes.
-	var n int
-	for n < len(str) {
-		r, s := utf8.DecodeRune(str)
-		if unicode.IsPrint(r) {
-			n += s
-		} else {
-			copy(str[n:], str[n+s:])
-			str = str[:len(str)-s]
+	if state == key.Press {
+		var n int
+		for n < len(str) {
+			r, s := utf8.DecodeRune(str)
+			if unicode.IsPrint(r) {
+				n += s
+			} else {
+				copy(str[n:], str[n+s:])
+				str = str[:len(str)-s]
+			}
 		}
-	}
-	if state == key.Press && len(str) > 0 {
-		events = append(events, key.EditEvent{Text: string(str)})
+		if len(str) > 0 {
+			events = append(events, key.EditEvent{Text: string(str)})
+		}
 	}
 	return
 }
