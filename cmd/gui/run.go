4ed7808ea6c482ec0b99f806a04326efd181ff1c
Loki <stalker.loki@protonmail.ch>
2020-12-02 09:13:38 +0100

    starts miner without shell

--- cmd/gui/run.go
+++ cmd/gui/run.go
@@ -64,11 +64,11 @@ func (wg *WalletGUI) Runner() (err error) {
 					*wg.cx.Config.NodeOff = true
 					*wg.cx.Config.WalletOff = true
 					save.Pod(wg.cx.Config)
-					if wg.mining {
-						go func() {
-							wg.MinerRunCommandChan <- "stop"
-						}()
-					}
+					// if wg.mining {
+					// 	go func() {
+					// 		wg.MinerRunCommandChan <- "stop"
+					// 	}()
+					// }
 					wg.running = false
 				case "restart":
 					Debug("restart called")
@@ -83,11 +83,11 @@ func (wg *WalletGUI) Runner() (err error) {
 				switch cmd {
 				case "run":
 					Debug("run called for miner")
-					if wg.running == false {
-						Debug("not running because shell is not running")
-						wg.mining = false
-						break
-					}
+					// if wg.running == false {
+					// 	Debug("not running because shell is not running")
+					// 	wg.mining = false
+					// 	break
+					// }
 					if *wg.cx.Config.GenThreads == 0 {
 						wg.mining = false
 						break
