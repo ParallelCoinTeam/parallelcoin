f8a477d8af888c00e6842c6f8927a9a73a08c3f2
Loki Verloren <stalker.loki@protonmail.ch>
2021-02-11 11:35:58 +0100

    and now also nice and tidy

--- pkg/util/routeable/routeable.go
+++ pkg/util/routeable/routeable.go
@@ -2,7 +2,6 @@ package routeable
 
 import (
 	"errors"
-	"github.com/davecgh/go-spew/spew"
 	"net"
 	"strings"
 	
@@ -51,108 +50,44 @@ func Discover() (err error) {
 			Debugs(nif[i])
 		}
 	} else {
+		var gw net.IP
 		if Gateway != nil {
-			// Debug("default gateway:", Gateway)
 			gws := Gateway.String()
-			gw := net.ParseIP(gws)
-			// out:
-			for i := range nif {
-				// Debugs(nif[i])
-				var addrs []net.Addr
-				if addrs, err = nif[i].Addrs(); Check(err) || addrs == nil {
+			gw = net.ParseIP(gws)
+		}
+		for i := range nif {
+			var addrs []net.Addr
+			if addrs, err = nif[i].Addrs(); Check(err) || addrs == nil {
+				continue
+			}
+			for j := range addrs {
+				var in *net.IPNet
+				if _, in, err = net.ParseCIDR(addrs[j].String()); Check(err) {
 					continue
 				}
-				for j := range addrs {
-					var in *net.IPNet
-					if _, in, err = net.ParseCIDR(addrs[j].String()); Check(err) {
-						continue
-					}
-					if in.Contains(gw) {
-						// Debug("network connection reachable from internet:", nif[i].Name, addrs[j].String())
-						Address = net.ParseIP(strings.Split(addrs[j].String(), "/")[0])
-						// Debugs(Address)
-						Interface = &nif[i]
-						// Debugs(Interface)
-						// break out
-					} else {
-						// add them to the other address/interface lists
-						// Debug(
-						// 	"network connection not reachable (directly) from the internet",
-						// 	nif[i].Name,
-						// 	addrs[j].String(),
-						// )
-						ip, _, _ := net.ParseCIDR(addrs[j].String())
-						SecondaryAddresses = append(SecondaryAddresses, ip)
-						SecondaryInterfaces = append(SecondaryInterfaces, &nif[i])
-					}
+				if Gateway != nil && in.Contains(gw) {
+					Address = net.ParseIP(strings.Split(addrs[j].String(), "/")[0])
+					Interface = &nif[i]
+					continue
 				}
+				ip, _, _ := net.ParseCIDR(addrs[j].String())
+				SecondaryAddresses = append(SecondaryAddresses, ip)
+				SecondaryInterfaces = append(SecondaryInterfaces, &nif[i])
 			}
 		}
 	}
 	Debug("Gateway", Gateway)
 	Debug("Address", Address)
-	Debug("Interface", spew.Sdump(Interface))
-	Debug("SecondaryAddresses", spew.Sdump(SecondaryAddresses))
-	Debug("SecondaryInterfaces", spew.Sdump(SecondaryInterfaces))
+	Debug("Interface", Interface.Name)
+	Debug("SecondaryAddresses")
+	for i := range SecondaryInterfaces {
+		Debug(SecondaryInterfaces[i].Name, SecondaryAddresses[i].String())
+	}
 	return
 }
 
 // GetInterface returns the address and interface of multicast capable interfaces
 func GetInterface() (ifc *net.Interface, address string, err error) {
-	// var err error
-	// var nif []net.Interface
-	// nif, err = net.Interfaces()
-	// if err != nil {
-	// 	Error("error:", err)
-	// }
-	// // // Traces(interfaces)
-	// // for ifi := range interfaces {
-	// // 	if interfaces[ifi].Flags&net.FlagLoopback == 0 && interfaces[ifi].
-	// // 		HardwareAddr != nil {
-	// // 		// iads, _ := interfaces[ifi].Addrs()
-	// // 		// for i := range iads {
-	// // 		//	//Traces(iads[i].Network())
-	// // 		// }
-	// // 		// Debug(interfaces[ifi].MulticastAddrs())
-	// // 		lanInterface = append(lanInterface, &interfaces[ifi])
-	// // 	}
-	// // }
-	// var routeableAddress string
-	// for i := range nif {
-	// 	// Debug(nif[i].Addrs())
-	// 	// Debug(nif[i].HardwareAddr)
-	// 	// filter out known virtual devices
-	// 	// microsoft hyper-v virtual interface
-	// 	if strings.HasPrefix(nif[i].HardwareAddr.String(), "00:15:5d") {
-	// 		continue
-	// 	}
-	// 	// todo: below here add discovered useful non-physical network interface tests like the one above
-	// 	addrs, _ := nif[i].Addrs()
-	// 	// Debug(addrs)
-	// 	for j := range addrs {
-	// 		// Debug(addresses[i].String())
-	// 		if !strings.ContainsAny(addrs[j].String(), ":") {
-	// 			routeableAddress = strings.Split(addrs[j].String(), "/")[0]
-	// 			// all addresses except localhost can exit potentially to the internet, on linux often these show first
-	// 			if strings.HasPrefix(routeableAddress, "127") {
-	// 				continue
-	// 			}
-	// 			if routeableAddress != "" {
-	// 				address = append(address, routeableAddress)
-	//
-	// 			}
-	// 			break
-	// 		}
-	// 	}
-	// 	// Debug(addresses)
-	// 	if len(address) > 0 {
-	// 		ifc = append(ifc, nif[i])
-	// 	}
-	// }
-	// if routeableAddress == "" {
-	// 	panic("no network available")
-	// }
-	// // Traces(lanInterface)
 	if Address == nil || Interface == nil {
 		if Discover() != nil {
 			err = errors.New("no routeable address found")
@@ -165,23 +100,6 @@ func GetInterface() (ifc *net.Interface, address string, err error) {
 }
 
 func GetListenable() net.IP {
-	// // first add the interface addresses
-	// rI, _ := GetInterface()
-	// var lA []net.TCPAddr
-	// for i := range rI {
-	// 	l, err := rI[i].Addrs()
-	// 	if err != nil {
-	// 		Error(err)
-	// 		return nil
-	// 	}
-	// 	for j := range l {
-	// 		ljs := l[j].String()
-	// 		ip := net.ParseIP(ljs)
-	// 		lA = append(
-	// 			lA, net.TCPAddr{IP: ip},
-	// 		)
-	// 	}
-	// }
 	if Address == nil {
 		if Discover() != nil {
 			Error("no routeable address found")
