4d7316c0de0d7f3a95f814dbc222686cb216a84c
Loki Verloren <stalker.loki@protonmail.ch>
2021-04-30 19:40:39 +0200

    much too slow making all of them run generate since anyway I already did

--- pod/podbuild/commands.go
+++ pod/podbuild/commands.go
@@ -2,53 +2,33 @@ package main
 
 var commands = map[string][]string{
 	"build": {
-		"go install -v ./pod/podbuild/.",
-		"podbuild generate",
 		"go build -v ./pod/.",
 	},
 	"install": {
-		"go install -v ./pod/podbuild/.",
-		"podbuild generate",
 		"go install -v ./pod/.",
 	},
 	"gui": {
-		"go install -v ./pod/podbuild/.",
-		"podbuild generate",
 		"go run -v ./pod/. gui",
 	},
 	"node": {
-		"go install -v ./pod/podbuild/.",
-		"podbuild generate",
 		"go run -v ./pod/. node",
 	},
 	"wallet": {
-		"go install -v ./pod/podbuild/.",
-		"podbuild generate",
 		"go run -v ./pod/.",
 	},
 	"kopach": {
-		"go install -v ./pod/podbuild/.",
-		"podbuild generate",
 		"go run -v ./pod/.",
 	},
 	"headless": {
-		"go install -v ./pod/podbuild/.",
-		"podbuild generate",
 		"go install -v -tags headless ./pod/.",
 	},
 	"docker": {
-		"go install -v ./pod/podbuild/.",
-		"podbuild generate",
 		"go install -v -tags headless ./pod/.",
 	},
 	"appstores": {
-		"go install -v ./pod/podbuild/.",
-		"podbuild generate",
 		"go install -v -tags nominers ./pod/.",
 	},
 	"tests": {
-		"go install -v ./pod/podbuild/.",
-		"podbuild generate",
 		"go test ./...",
 	},
 	"builder": {
